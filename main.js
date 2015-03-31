99cbf74451efeddf68d385e7442b864ae8e75d57
adorsk <adorsk@gmail.com>
2015-03-31 10:17:36 -0400

    start of output

--- main.js
+++ main.js
@@ -15,7 +15,51 @@ $(document).ready(function() {
   })
 
   promise.then(function(data) {
-    console.log('data is: ', data);
+    // Group items by date.
+    var itemsByDate = {};
+    var items = data.items;
+    for (var i=0; i < items.length; i++) {
+      var item = items[i];
+      var startDate = new Date(item.start.dateTime || item.start.date);
+      var dateString = startDate.toDateString();
+      if (! itemsByDate[dateString]) {
+        itemsByDate[dateString] = [];
+      }
+      itemsByDate[dateString].push(item);
+    }
+
+    // Format output.
+    var $outputEl = $('#output');
+    var $datesListEl = $('<ol></ol>');
+    for (var dateString in itemsByDate) {
+      var $dateLiEl = $('<li>' + dateString  + '</li>');
+      $datesListEl.append($dateLiEl);
+
+      var $itemsListEl = $('<ol></ol>');
+
+      var dateItems = itemsByDate[dateString];
+      dateItems.forEach(function(item) {
+        var $itemLiEl = $('<li></li>');
+
+        // Format time if available.
+        var time = '';
+        if (item.start.dateTime) {
+          var dateTime = new Date(item.start.dateTime);
+          var time = pad(dateTime.getHours(),2) + ':' + pad(dateTime.getMinutes(),2);
+        }
+
+        var title = item.summary.toUpperCase();
+
+        $itemLiEl.html(time + ' ' + title + '</br>' + item.description);
+
+        $itemsListEl.append($itemLiEl);
+      });
+
+      $dateLiEl.append($itemsListEl);
+    }
+
+    $outputEl.append($datesListEl);
+
   });
 
   promise.error(function(xhr, status, e) {
@@ -23,3 +67,8 @@ $(document).ready(function() {
   });
 });
 
+// Helper function for padding w/ zeros.
+function pad (str, max) {
+  str = str.toString();
+  return str.length < max ? pad("0" + str, max) : str;
+}
